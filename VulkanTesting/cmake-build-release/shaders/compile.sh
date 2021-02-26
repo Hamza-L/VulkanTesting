2ef25e902dbd3604dbc20a928c99b57e84fbff2b
Hamza Lahmimsi <hlahmimsi@gmail.com>
2021-02-26 11:43:21 -0500

    small changes

---
+++ VulkanTesting/cmake-build-release/shaders/compile.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+
+#  compile.sh
+#  VulkantTesting
+#
+#  Created by Hamza Lahmimsi on 2021-02-13.
+#  
+
+/Users/hamzalah/VulkanSDK/macOS/bin/glslc shader.vert -o vert.spv
+/Users/hamzalah/VulkanSDK/macOS/bin/glslc shader.frag -o frag.spv
