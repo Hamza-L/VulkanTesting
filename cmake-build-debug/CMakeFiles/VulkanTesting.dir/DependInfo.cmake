c0c2fb5ff02263a45930fbc15fe9a985f3649cd1
Hamza Lahmimsi <hlahmimsi@gmail.com>
2021-03-12 13:51:57 -0500

    small changes

---
+++ cmake-build-debug/CMakeFiles/VulkanTesting.dir/DependInfo.cmake
@@ -0,0 +1,28 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/Users/hamzalah/Documents/gitProjects/VulkanTest/VulkanTesting/VulkanTesting/main.cpp" "/Users/hamzalah/Documents/gitProjects/VulkanTest/VulkanTesting/VulkanTesting/cmake-build-debug/CMakeFiles/VulkanTesting.dir/main.cpp.o"
+  "/Users/hamzalah/Documents/gitProjects/VulkanTest/VulkanTesting/VulkanTesting/newVulkanApp.cpp" "/Users/hamzalah/Documents/gitProjects/VulkanTest/VulkanTesting/VulkanTesting/cmake-build-debug/CMakeFiles/VulkanTesting.dir/newVulkanApp.cpp.o"
+  "/Users/hamzalah/Documents/gitProjects/VulkanTest/VulkanTesting/VulkanTesting/vulkanDevice.cpp" "/Users/hamzalah/Documents/gitProjects/VulkanTest/VulkanTesting/VulkanTesting/cmake-build-debug/CMakeFiles/VulkanTesting.dir/vulkanDevice.cpp.o"
+  "/Users/hamzalah/Documents/gitProjects/VulkanTest/VulkanTesting/VulkanTesting/vulkanModel.cpp" "/Users/hamzalah/Documents/gitProjects/VulkanTest/VulkanTesting/VulkanTesting/cmake-build-debug/CMakeFiles/VulkanTesting.dir/vulkanModel.cpp.o"
+  "/Users/hamzalah/Documents/gitProjects/VulkanTest/VulkanTesting/VulkanTesting/vulkanPipeline.cpp" "/Users/hamzalah/Documents/gitProjects/VulkanTest/VulkanTesting/VulkanTesting/cmake-build-debug/CMakeFiles/VulkanTesting.dir/vulkanPipeline.cpp.o"
+  "/Users/hamzalah/Documents/gitProjects/VulkanTest/VulkanTesting/VulkanTesting/vulkanSwapChain.cpp" "/Users/hamzalah/Documents/gitProjects/VulkanTest/VulkanTesting/VulkanTesting/cmake-build-debug/CMakeFiles/VulkanTesting.dir/vulkanSwapChain.cpp.o"
+  "/Users/hamzalah/Documents/gitProjects/VulkanTest/VulkanTesting/VulkanTesting/vulkanWindow.cpp" "/Users/hamzalah/Documents/gitProjects/VulkanTest/VulkanTesting/VulkanTesting/cmake-build-debug/CMakeFiles/VulkanTesting.dir/vulkanWindow.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "AppleClang")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "/usr/local/include"
+  "/Users/hamzalah/VulkanSDK/macOS/include"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
